ee6926a318f210a4afc9af6bebf694be082462b7
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-07-14 19:37:35 -0300

    refactor: ensure SignUpController.handle implement correct interfaces

--- src/presentation/controllers/signUp.ts
+++ src/presentation/controllers/signUp.ts
@@ -1,18 +1,22 @@
+import { HttpResponse, HttpRequest } from '../protocols/http';
+
 class SignUpController {
-  static handle(httpRequest: any): any {
+  static handle(httpRequest: HttpRequest): HttpResponse {
+    let httpResponse = {} as HttpResponse;
     if (!httpRequest.body.name) {
-      return {
+      httpResponse = {
         statusCode: 400,
         body: new Error('Missing param: name'),
       };
     }
     if (!httpRequest.body.email) {
-      return {
+      httpResponse = {
         statusCode: 400,
         body: new Error('Missing param: email'),
       };
     }
-    return true;
+
+    return httpResponse;
   }
 }
 
