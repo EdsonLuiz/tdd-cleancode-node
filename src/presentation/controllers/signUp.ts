a017ecad7829289a9efc48ce0f2483d277bf343e
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-07-17 22:58:12 -0300

    feat: ensure SignUpController returns 400 if an invalid email is provided

--- src/presentation/controllers/signUp.ts
+++ src/presentation/controllers/signUp.ts
@@ -2,8 +2,16 @@ import { HttpResponse, HttpRequest } from '../protocols/http';
 import { MissingParamError } from '../errors/missing-param-error';
 import { badRequest } from '../helpers/http-helper';
 import { Controller } from '../protocols/controller';
+import { EmailValidator } from '../protocols/email-validator';
+import { InvalidParamError } from '../errors/invalid-param-error';
 
 class SignUpController implements Controller {
+  private readonly emailValidator: EmailValidator;
+
+  constructor(emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator;
+  }
+
   handle(httpRequest: HttpRequest): HttpResponse {
     let httpResponse = {} as HttpResponse;
 
@@ -13,6 +21,9 @@ class SignUpController implements Controller {
       if (!httpRequest.body[field]) return badRequest(new MissingParamError(field));
       return _httpResponse;
     }, {} as HttpResponse);
+
+    const isValid = this.emailValidator.isValid(httpRequest.body.email);
+    if (!isValid) httpResponse = badRequest(new InvalidParamError('email'));
 
     return httpResponse;
   }
