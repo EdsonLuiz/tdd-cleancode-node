af45c08d8c2ae4347d2ae9bf6937e440f95a35c2
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-08-01 20:24:33 -0300

    feat: ensure requests return json as default content type

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express';
 import { bodyParser } from '@/main/middlewares/body-parser';
 import { cors } from '@/main/middlewares/cors';
+import { contentType } from '@/main/middlewares/content-type';
 
 export default (app: Express): void => {
   app.use(cors);
+  app.use(contentType);
   app.use(bodyParser);
 };
