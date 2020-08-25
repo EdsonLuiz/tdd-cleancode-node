81ba4cee66369df1f81d0a061c3c7696576fe2aa
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-08-25 20:53:31 -0300

    refactor: move sut creation to jest beforeEach method

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -2,9 +2,13 @@ import { badRequest } from '@/presentation/helpers/http-helper';
 import { MissingParamError } from '@/presentation/errors';
 import { LoginController } from './login';
 
+let sut: LoginController;
+
 describe('Login Controller', () => {
+  beforeEach(() => {
+    sut = new LoginController();
+  });
   it('Should return 400 if no email is provided', async () => {
-    const sut = new LoginController();
     const httpRequest = {
       body: {
         password: 'valid_password',
@@ -16,7 +20,6 @@ describe('Login Controller', () => {
   });
 
   it('Should return 400 if no password is provided', async () => {
-    const sut = new LoginController();
     const httpRequest = {
       body: {
         email: 'valid@email.com',
