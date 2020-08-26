e6f542dd5b984367e3828bee0d95fec2c9b0fdc4
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-08-26 20:12:15 -0300

    feat: ensure LoginController returns 500 if EmailValidator throws

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,5 +1,5 @@
 import { HttpRequest, HttpResponse, Controller } from '@/presentation/protocols/';
-import { badRequest } from '@/presentation/helpers/http-helper';
+import { badRequest, serverError } from '@/presentation/helpers/http-helper';
 import { MissingParamError, InvalidParamError } from '@/presentation/errors';
 import { EmailValidator } from '../signUp/signup-protocols';
 
@@ -7,18 +7,22 @@ export class LoginController implements Controller {
   constructor(private readonly emailValidator: EmailValidator) {}
 
   public async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { email, password } = httpRequest.body;
-    if (!email) {
-      return new Promise((resolve) => resolve(badRequest(new MissingParamError('email'))));
-    }
-    if (!password) {
-      return new Promise((resolve) => resolve(badRequest(new MissingParamError('password'))));
-    }
-    const isValid = this.emailValidator.isValid(email);
+    try {
+      const { email, password } = httpRequest.body;
+      if (!email) {
+        return new Promise((resolve) => resolve(badRequest(new MissingParamError('email'))));
+      }
+      if (!password) {
+        return new Promise((resolve) => resolve(badRequest(new MissingParamError('password'))));
+      }
+      const isValid = this.emailValidator.isValid(email);
 
-    if (!isValid) {
-      return new Promise((resolve) => resolve(badRequest(new InvalidParamError('email'))));
+      if (!isValid) {
+        return new Promise((resolve) => resolve(badRequest(new InvalidParamError('email'))));
+      }
+      return new Promise((resolve) => resolve({} as HttpResponse));
+    } catch (error) {
+      return serverError(error);
     }
-    return new Promise((resolve) => resolve({} as HttpResponse));
   }
 }
